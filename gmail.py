9e7e3d843ca65639317b915bd25e4cfa35c3b094
Razeen Faruque <razeenfaruque@gmail.com>
2022-10-25 20:32:42 -0400

    added email

---
+++ gmail.py
@@ -0,0 +1,47 @@
+import httplib2
+import os
+import oauth2client
+from oauth2client import client, tools, file
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from apiclient import errors, discovery
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.send'
+CLIENT_SECRET_FILE = 'client_secret.json'
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'gmail-python-email-send.json')
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        credentials = tools.run_flow(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def createMessage(recipient, username, title, price, listedPrice, discount, asin):
+    msg = MIMEMultipart()
+    msg['Subject']  = "An item you're tracking is on sale!"
+    msg['To'] = recipient
+    msg['From'] = 'Price Tracker Bot <amzn.tracker.bot@gmail.com>'
+    html = open('template.html', 'r').read().format(**locals())
+    msg.attach(MIMEText(html, 'html'))
+    raw = base64.urlsafe_b64encode(msg.as_bytes()).decode()
+    email = {'raw': raw}
+    sendMessage(email)
+
+def sendMessage(email):
+    # creates gmail api instance 
+    http = get_credentials().authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http)
+    try:
+        message = (service.users().messages().send(userId="me", body=email).execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+from dis import disco
+from turtle import title
+from webbrowser import get
 from requests_html import HTMLSession
 import json
 import re
+from gmail import createMessage
+
+def getData():
+    with open('data.json') as file:
+        data = json.load(file)
+    return data
+
+def saveData(data):
+    with open('data.json', 'w') as file:
+        json.dump(data, file, indent=4)
 
 def scrape(asin):
     session = HTMLSession()
     page = session.get(f'https://www.amazon.ca/gp/product/{asin}')
-    page.html.render(sleep=1)  
+    page.html.render(timeout=20)  
     title = page.html.find('#productTitle')[0].text    
     try:
         price = page.html.find('.a-offscreen')[0].text.replace('$','').strip()
@@ -14,13 +27,11 @@ def scrape(asin):
         fraction = page.html.find('.a-price-fraction')[0].text
         price = whole+fraction
     print(title, price)
-    return price
+    return price, title
 
 def track(link, email, username):
     newAsin = re.search(r'/[dg]p/([^/]+)', link, flags=re.IGNORECASE).group(1)
-    with open('data.json') as file:
-        data = json.load(file)
-
+    data = getData()
     if newAsin in data["ASIN"] and username not in data["ASIN"][newAsin][0]["usernames"]:
         data["ASIN"][newAsin][0]["emails"].append(email)
         data["ASIN"][newAsin][0]["usernames"].append(username)
@@ -38,33 +49,35 @@ def track(link, email, username):
             }
         ]}
         data["ASIN"].update(info)
+    saveData(data)
 
-    with open('data.json', 'w') as file:
-        json.dump(data, file, indent=4)
-
-def notify(asin):
-    # use gmail api here 
-    with open("data.json") as file:
-        data = json.load(file)
+def notify(asin, title, price, listedPrice, discount):
+    data = getData()
+    emailList = data["ASIN"][asin][0]["emails"]
+    index = 0
+    for email in emailList:
+        username = data["ASIN"][asin][0]["usernames"][index]
+        createMessage(email, username, title, price, listedPrice, discount, asin) 
+        index+=1
     del data["ASIN"][asin]
-    with open("data.json", "w") as file:
-        json.dump(data, file, indent=4)
+    saveData(data)
 
-def comparePrice(price, asin):
+def comparePrice(price_title, asin):
+    price = price_title[0]
+    title = price_title[1]
+    data = getData()
     listedPrice = data["ASIN"][asin][0]["price"]
     if float(price) < listedPrice:
         discount = int(((listedPrice-float(price))/listedPrice)*100)
-        print(f"Product was {listedPrice}, now on sale for {price}!! That's a savings of {discount}%.")
-        notify(asin)
+        print("On sale. Sending out emails...")
+        notify(asin, title, price, listedPrice, discount)
     else:
         print("No sale.")
 
-with open('data.json') as file:
-    data = json.load(file)
-
 def main():
+    data = getData()
     for asin in data["ASIN"]:
-        price = scrape(asin)
-        comparePrice(price, asin)
+        price_title = scrape(asin)
+        comparePrice(price_title, asin)
 
 main()
